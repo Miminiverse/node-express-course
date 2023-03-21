208df5dee6a811e64949ba4646a9e37a3e69ccc8
Minh Tam Pham <miminiverse@Minhs-MBP.fios-router.home>
2023-03-20 22:15:15 -0400

    week2-edit

--- 01-node-tutorial/answers/17-http-stream.js
+++ 01-node-tutorial/answers/17-http-stream.js
@@ -1,18 +1,5 @@
-var fs = require('fs');
-var http = require('http');
-
-// http
-//     .createServer(function (req, res) {
-//     const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
-//     fileStream.on('open', () => {
-//         fileStream.pipe(res)
-//     })
-//     fileStream.on("error", (err) => {
-//         res.end(err)
-//     })
-// })
-
-//     .listen(5000)
+var http = require('http')
+var fs = require('fs')
 
 http
   .createServer(function (req, res) {
