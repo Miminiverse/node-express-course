a20d26bdd313336c92da21ee2b089b904c552913
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-04-29 20:32:38 -0400

    week7

---
+++ 04-store-api/starter/routes/products.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const {getAllProductStatic,getAllProduct} = require('../controllers/products')
+
+router.route('/').get(getAllProduct)
+router.route('/static').get(getAllProductStatic)
+
+module.exports = router
